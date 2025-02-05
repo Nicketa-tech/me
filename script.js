4e907afa677597cae5452428e405d10f830a2008
Nicketa <30273897+nicks9188@users.noreply.github.com>
2025-02-05 13:09:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Open the sidebar */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Close the sidebar */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
